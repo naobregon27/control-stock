dd25a89a79d9c385ac992b1808ea9a32d262cea2
naobregon27 <na.obregon27@hotmail.com>
2024-08-10 13:27:04 -0300

    OK

--- cliente/src/pages/registro.jsx
+++ cliente/src/pages/registro.jsx
@@ -73,30 +73,32 @@ const Registro = () => {
         <div className="w-full max-w-4xl">
           <div className="bg-green-500 text-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <h3 className="text-2xl font-bold my-3">Lista de Ventas</h3>
-            <table className="min-w-full bg-white border border-gray-400">
-              <thead>
-                <tr className="bg-teal-500 text-white">
-                  <th className="py-2 px-4 border-b">Fecha de Registro</th>
-                  <th className="py-2 px-4 border-b">Nombre del Producto</th>
-                  <th className="py-2 px-4 border-b">Precio</th>
-                  <th className="py-2 px-4 border-b">Cantidad</th>
-                  <th className="py-2 px-4 border-b">Cliente</th>
-                  <th className="py-2 px-4 border-b">Documento del Cliente</th>
-                </tr>
-              </thead>
-              <tbody>
-                {ventas.map((venta, index) => (
-                  <tr key={index} className="hover:bg-gray-100">
-                    <td className="py-2 px-4 border-b text-black">{venta.fechaRegistro}</td>
-                    <td className="py-2 px-4 border-b text-black">{venta.nombreProducto}</td>
-                    <td className="py-2 px-4 border-b text-black">${venta.precio}</td>
-                    <td className="py-2 px-4 border-b text-black">{venta.cantidad}</td>
-                    <td className="py-2 px-4 border-b text-black">{venta.nombreCliente}</td>
-                    <td className="py-2 px-4 border-b text-black">{venta.documentoCliente}</td>
+            <div className="overflow-x-auto">
+              <table className="min-w-full bg-white border border-gray-400">
+                <thead>
+                  <tr className="bg-teal-500 text-white">
+                    <th className="py-2 px-4 border-b">Fecha de Registro</th>
+                    <th className="py-2 px-4 border-b">Nombre del Producto</th>
+                    <th className="py-2 px-4 border-b">Precio</th>
+                    <th className="py-2 px-4 border-b">Cantidad</th>
+                    <th className="py-2 px-4 border-b">Cliente</th>
+                    <th className="py-2 px-4 border-b">Documento del Cliente</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {ventas.map((venta, index) => (
+                    <tr key={index} className="hover:bg-gray-100">
+                      <td className="py-2 px-4 border-b text-black">{venta.fechaRegistro}</td>
+                      <td className="py-2 px-4 border-b text-black">{venta.nombreProducto}</td>
+                      <td className="py-2 px-4 border-b text-black">${venta.precio}</td>
+                      <td className="py-2 px-4 border-b text-black">{venta.cantidad}</td>
+                      <td className="py-2 px-4 border-b text-black">{venta.nombreCliente}</td>
+                      <td className="py-2 px-4 border-b text-black">{venta.documentoCliente}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
 
             <div className="mt-4">
               <label className="block text-sm font-bold mb-2">
